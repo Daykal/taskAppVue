100f62d4271b3b1559a7b33a9befc3f1c696d862
MahdiMac <mahdijj0090@gmail.com>
2026-05-19 12:53:16 +0200

    Add Navbar install tailwind4

--- taskApp-vue/src/App.vue
+++ taskApp-vue/src/App.vue
@@ -1,6 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navbar from './components/Navbar.vue';
+
+</script>
 
 <template>
+  <Navbar></Navbar>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
---
+++ taskApp-vue/src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="bg-blue-700 border-b border-blue-500">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="flex h-20 items-center justify-center">
+        <span class="text-white font-bold text-3xl"> Task App Vue</span>
+      </div>
+    </div>
+  </nav>
+</template>
